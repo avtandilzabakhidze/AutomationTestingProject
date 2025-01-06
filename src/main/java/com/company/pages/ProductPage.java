7bf6154764e916a73fddc878c8469ec290417985
avtandilzabakhidze <zabakhidze01@gmail.com>
2025-01-06 21:13:31 +0400

    add ProductPage

--- src/main/java/com/company/pages/ProductPage.java
+++ src/main/java/com/company/pages/ProductPage.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ProductPage extends BasePage {
@@ -14,6 +18,7 @@ public class ProductPage extends BasePage {
     private final By productName = By.xpath("//div[@class ='inventory_item_name ']");
     private final By cart = By.xpath("//a[@class ='shopping_cart_link']");
     private final By cartProductNumber = By.xpath("//span[@class ='shopping_cart_badge']");
+    private final By sortButton = By.xpath("//select[@class=\"product_sort_container\"]");
 
     public ProductPage(WebDriver driver) {
         super(driver);
@@ -47,5 +52,45 @@ public class ProductPage extends BasePage {
 
     public boolean checkProductNumber() {
         return elementIsDisplayed(cartProductNumber);
+    }
+
+    public String getProductPrice() {
+        return getText(price);
+    }
+
+    public String getProductName() {
+        return getText(productName);
+    }
+
+    public List<String> getProductNames() {
+        List<WebElement> productElementsName = findElements(productName);
+        List<String> productNames= new ArrayList<>();
+        for (WebElement product : productElementsName) {
+            productNames.add(product.getText());
+        }
+        return productNames;
+    }
+
+    public void sortByNameAsc(){
+        Select select = new Select(findElement(sortButton));
+        select.selectByValue("az");
+    }
+
+    public List<Double> getProductPrices() {
+        List<WebElement> productPricesName = findElements(price);
+        List<Double> productPrices = new ArrayList<>();
+
+        for (WebElement product : productPricesName) {
+            String priceText = product.getText().replaceAll("[^\\d.]", "");
+            double price = Double.parseDouble(priceText);
+            productPrices.add(price);
+        }
+
+        return productPrices;
+    }
+
+    public void sortByPriceToHigh(){
+        Select select = new Select(findElement(sortButton));
+        select.selectByValue("lohi");
     }
 }
--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -1,6 +1,11 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class ProductTest extends BaseTest {
     @Test
     public void addFirstProduct() {
@@ -35,5 +40,39 @@ public class ProductTest extends BaseTest {
         productPage.removeAllProductsFromCart();
 
         Assert.assertFalse(productPage.checkProductNumber(), "\n product number is not correct \n");
+    }
+
+    @Test
+    public void verifyProductDetails(){
+        baseLogin();
+        String productPrice = productPage.getProductPrice();
+        String productName = productPage.getProductName();
+
+        Assert.assertEquals(productPrice,"$29.99","\n product price is not correct \n");
+        Assert.assertEquals(productName,"Sauce Labs Backpack","\n product name is not correct \n");
+    }
+
+    @Test
+    public void testSortByNameAToZ(){
+        baseLogin();
+        productPage.sortByNameAsc();
+
+        List<String> productNames = productPage.getProductNames();
+        List<String> sortedProductNames = new ArrayList<>(productNames);
+        Collections.sort(sortedProductNames);
+
+        Assert.assertEquals(sortedProductNames,productNames,"\n Sort A ro Z don't work \n");
+    }
+
+    @Test
+    public void testSortByPriceLowToHigh(){
+        baseLogin();
+        productPage.sortByPriceToHigh();
+
+        List<Double> productPrices = productPage.getProductPrices();
+        List<Double> sortedProductPrices= new ArrayList<>(productPrices);
+        Collections.sort(sortedProductPrices);
+
+        Assert.assertEquals(sortedProductPrices,productPrices,"\n Sort Low to High don't work \n");
     }
 }
