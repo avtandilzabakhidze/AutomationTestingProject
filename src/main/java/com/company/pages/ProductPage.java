2c32f710983a57f47a6af50a96e5a98382c3fad2
avtandilzabakhidze <zabakhidze01@gmail.com>
2025-01-18 18:01:30 +0400

    Delete unnecessary files

--- src/main/java/com/company/pages/ProductPage.java
+++ src/main/java/com/company/pages/ProductPage.java
@@ -1,14 +1,11 @@
 package com.company.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class ProductPage extends BasePage {
